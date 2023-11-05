6d9b9ce109bff726e6e8b27a4a2b37e622081612
FabienBaraille <fabien.baraille@oclock.school>
2023-11-05 14:19:18 +0100

    adjustement

--- App/views/main/pokemon_list.tpl.php
+++ App/views/main/pokemon_list.tpl.php
@@ -1,9 +1,11 @@
 <section class="wrapper home">
-<?php foreach ($allPokemons as $pokemon) : 
-    $link = $router->generate("page_detail",["id" => $pokemon->getNumber()])?>
-    <div class="pokemon-card">
-        <img src="<?= $absoluteURL ?>/img/<?= $pokemon->getNumber() ?>.png" alt="<?= $pokemon->getName() ?>">
-        <a id="link-anim" href="<?= $link ?>" data-url="<?= $link ?>">#<?= $pokemon->getNumber() ?> <?= $pokemon->getName() ?></a>
-    </div>
-<?php endforeach; ?>
+    <?php foreach ($allPokemons as $pokemon) : 
+        $link = $router->generate("page_detail",["id" => $pokemon->getNumber()])?>
+        <div class="pokemon-card">
+            <a id="link-anim" href="<?= $link ?>" data-url="<?= $link ?>">
+            <img src="<?= $absoluteURL ?>/img/<?= $pokemon->getNumber() ?>.png" alt="<?= $pokemon->getName() ?>">
+            #<?= $pokemon->getNumber() ?> <?= $pokemon->getName() ?>
+            </a>
+        </div>
+    <?php endforeach; ?>
 </section>
